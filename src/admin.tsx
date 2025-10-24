903ef2de6984057b27cf08832ee51341b571c1e2
Jimmy <chchenha@163.com>
2025-10-24 23:01:51 +0800

    first commit

---
+++ src/admin.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const AdminApp = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
+
+export default AdminApp;
