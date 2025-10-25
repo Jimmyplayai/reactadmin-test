0be5e740d6988947043ff853d588aa56120eadb8
Jimmy <chchenha@163.com>
2025-10-25 16:02:34 +0800

    add the login featrue

--- src/admin.tsx
+++ src/admin.tsx
@@ -1,10 +1,16 @@
 import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import authProvider from "./authProvider";
+import LoginPage from "./LoginPage";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const AdminApp = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    loginPage={LoginPage}
+  >
     <Resource name="posts" list={ListGuesser} />
     <Resource name="users" list={ListGuesser} />
   </Admin>
